250dd5bef8173ed40d36c02d53f1573c807db5c7
Michael Torpey <mct25@st-andrews.ac.uk>
2018-11-19 14:52:29 +0000

    preprocessing: remove default values

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,21 @@ from inspect import getfullargspec
 
 
 def arg_tuple(func, *args, **kwargs):
-    kwargs.update(dict(zip(getfullargspec(func).args, args)))
+    spec = getfullargspec(func)
+
+    # Convert args to kwargs
+    kwargs.update(dict(zip(spec.args, args)))
+
+    # Remove any default arguments
+    if not spec.defaults is None:
+        for (arg, val) in zip(spec.args[-len(spec.defaults):], spec.defaults):
+            if kwargs.get(arg) == val:
+                kwargs.pop(arg)
+    if not spec.kwonlydefaults is None:
+        for arg in spec.kwonlydefaults:
+            if kwargs.get(arg) == spec.kwonlydefaults[arg]:
+                kwargs.pop(arg)
+
+    # Return as a tuple
     out = tuple(sorted(kwargs.items()))
     return out
