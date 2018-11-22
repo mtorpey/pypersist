efc176b392cc4985122ae52ec1b7dac3ad1fee6a
Michael Torpey <mct25@st-andrews.ac.uk>
2018-11-22 11:15:37 +0000

    Use 'is not'

--- preprocessing.py
+++ preprocessing.py
@@ -14,11 +14,11 @@ def arg_tuple(func, *args, **kwargs):
     kwargs.update(dict(zip(spec.args, args)))
 
     # Remove any default arguments
-    if not spec.defaults is None:
+    if spec.defaults is not None:
         for (arg, val) in zip(spec.args[-len(spec.defaults):], spec.defaults):
             if kwargs.get(arg) == val:
                 kwargs.pop(arg)
-    if not spec.kwonlydefaults is None:
+    if spec.kwonlydefaults is not None:
         for arg in spec.kwonlydefaults:
             if kwargs.get(arg) == spec.kwonlydefaults[arg]:
                 kwargs.pop(arg)
