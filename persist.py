0b52075526cec9aefd3fabce06c1ba7637da7e3e
Michael Torpey <mct25@st-andrews.ac.uk>
2018-12-07 12:05:32 +0000

    persist: add docstring

--- persist.py
+++ persist.py
@@ -13,6 +13,85 @@ def persist(func=None,
             pickle=pickling.pickle,
             unpickle=pickling.unpickle,
             hash=hashing.hash):
+    """Function decorator for persistent memoisation
+
+    Store the output of a function permanently, and use previously stored
+    results instead of recomputing them.
+
+    To use this, decorate the desired function with `@persist`.
+    Or to customise the way this memoisation is done, decorate with
+    `@persist(<args>)` and specify custom parameters.
+
+    Parameters
+    ----------
+    basedir : str, optional
+        The directory in which results should be stored, possibly along with
+        results from other functions.  Default is 'persist'.
+    funcdir : str, optional
+        The directory inside `basedir` in which the results for just this
+        function should be stored.  Default is the name of the function.
+    key : function(args -> object), optional
+        Function that takes the arguments given to the memoised function, and
+        returns a key that uniquely identifies those arguments.  Two sets of
+        arguments should have the same key only if they produce the same output
+        when passed into the memoised function.  Default returns a sorted tuple
+        describing the arguments along with their names.
+    storekey : bool, optional
+        Whether to store the key along with the output when a result is stored.
+        If True, the key will be checked when recalling a previously computed
+        value, to check for hash collisions.  If False, two keys will produce
+        the same output whenever their `hash` values are the same.  Default is
+        False.
+    pickle : function(object -> string), optional
+        Function that converts the output of the function to a string for
+        storage.  Should be the inverse of `unpickle`.  If `storekey` is true,
+        this will also be used to store the key, and should do so without
+        newline characters.  Default uses the `pickle` module and base 64
+        encoding.
+    unpickle : function(string -> object), optional
+        Function that converts a string back to an object when retrieving a
+        computed value from storage.  Should be the inverse of `pickle`.  If
+        `storekey` is true, this will also be used to retrieve the key.
+        Default uses the `pickle` module and base 64 encoding.
+    hash : function(object -> string), optional
+        Function that takes a key and produces a string that will be used to
+        identify that key.  If this function is not injective, then `storekey`
+        should be set to True to check for hash collisions.  The string should
+        only contain characters safe for filenames.  Default uses SHA-1 and
+        base 64 encoding, which can store 10^22 objects with a <0.01% chance of
+        a collision.
+
+    Examples
+    --------
+    Simple persistence using default settings:
+
+    >>> @persist
+    ... def double(x):
+    ...     return 2 * x
+    >>> double(3)
+    6
+    >>> double(3)
+    6
+    >>> double.cache[(('x', 3),)]
+    6
+
+    Custom persistence using a simpler key, a descriptive filename, and writing
+    human-readable files:
+
+    >>> @persist(key=lambda x,y: (x,y),
+    ...          hash=lambda k: '%s_to_the_power_of_%s' % k,
+    ...          pickle=str,
+    ...          unpickle=int)
+    ... def power(x, y):
+    ...     return x ** y
+    >>> power(2,4)
+    16
+    >>> power(10,3)
+    1000
+    >>> power.cache[(2,4)]
+    16
+
+    """
 
     class persist_wrapper:
 
