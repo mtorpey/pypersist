38cd88d0c7197d76b7bced12cbc3a01275b5298a
Michael Torpey <mct25@st-andrews.ac.uk>
2019-01-25 17:25:48 +0000

    Change pip name to pypersist

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='pymemo',
+setup(name='pypersist',
       version='0.1',
       description='Persistent memoisation framework for Python',
       url='https://github.com/mtorpey/pymemo',
