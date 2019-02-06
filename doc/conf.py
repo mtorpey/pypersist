256c5395074d475664843863366b35106bf8ca39
Michael Torpey <mct25@st-andrews.ac.uk>
2019-02-06 13:03:36 +0000

    conf.py: correct syntax error

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('.')), 'pypersist')
+sys.path.insert(0,
+                os.path.join(os.path.dirname(os.path.abspath('.')),
+                             'pypersist'))
 
 # -- Project information -----------------------------------------------------
 
