8fff4605484a7b57e32d3a2cf2fd9f22f443e92d
Michael Torpey <mct25@st-andrews.ac.uk>
2019-02-06 13:05:19 +0000

    Back to old sys.path

--- doc/conf.py
+++ doc/conf.py
@@ -14,9 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0,
-                os.path.join(os.path.dirname(os.path.abspath('.')),
-                             'pypersist'))
+sys.path.insert(0, os.path.abspath('..'))
+
 
 # -- Project information -----------------------------------------------------
 
