ee666cf7c2de8f2667fb4201f00abb898a3c86ec
Michael Torpey <mct25@st-andrews.ac.uk>
2019-02-11 18:55:18 +0100

    tests: add chapters 1 and 2 from demo

--- tst/test_persist.py
+++ tst/test_persist.py
@@ -1,16 +1,48 @@
+import pytest
+
 from pypersist import persist
 
-import pypersist.preprocessing
+from time import sleep
+from os import listdir
 
-@persist
-def double(x):
-    return 2 * x
+def test_triple():
+    @persist
+    def triple(x):
+        return 3 * x
+    triple.clear()
 
-double.clear()
+    assert len(triple.cache) == 0
+    assert triple(3) == 9
+    assert len(triple.cache) == 1
+    assert triple(3) == 9
+    assert len(triple.cache) == 1
+    assert triple.cache
+    assert triple.cache[(('x', 3),)] == 9
+    triple.cache[(('x', 10),)] = 31
+    assert triple(10) == 31
+    with pytest.raises(KeyError) as ke:
+        print(triple.cache[(('x', 5),)])
+    assert ke.value.args[0] == (('x', 5),)
+    assert len(triple.cache) == 2
 
-def test_double():
-    assert len(double.cache) == 0
-    assert double(3) == 6
+def test_pickle():
+    @persist(pickle=repr, unpickle=eval)
+    def double(x):
+        return 2 * x
+    double.clear()
+
+    assert double(2) == 4
+    assert double(2) == 4
     assert len(double.cache) == 1
-    assert double.cache
-    assert double.cache[(('x', 3),)] == 6
+    assert double(x=2) == 4
+    assert len(double.cache) == 1
+    assert double(0) == 0
+    assert double('hello!') == 'hello!hello!'
+    assert len(double.cache) == 3
+
+    files = listdir('persist/double')
+    results = [open('persist/double/' + fname).read() for fname in files]
+    assert '4' in results
+    assert '0' in results
+    assert "'hello!hello!'" in results
+    assert 'some random string' not in results
