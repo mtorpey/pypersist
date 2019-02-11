a423c410648498fef749276fc053e1a8b3ea5f2f
Michael Torpey <mct25@st-andrews.ac.uk>
2019-02-11 08:49:58 +0000

    setup.py: require requests package

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(name='pypersist',
       author_email='mct25@st-andrews.ac.uk',
       license='GPL',
       packages=['pypersist'],
+      install_requires=['requests'],
       include_package_data=True,
       long_description=read('README.md'),
       zip_safe=False)
